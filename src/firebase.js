41e475200de77f50eee1379590fd031146a4ffc7
Raaya <rahulagre555@gmail.com>
2021-09-10 11:55:27 +0530

    created Sidebar and Chat component

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnHdR2SQJNi29V1LM87WY_nRuVlFVWzJY",
+  authDomain: "whatsapp-mern-71aca.firebaseapp.com",
+  projectId: "whatsapp-mern-71aca",
+  storageBucket: "whatsapp-mern-71aca.appspot.com",
+  messagingSenderId: "1046329980507",
+  appId: "1:1046329980507:web:c742af12830fee3bf9317e",
+  measurementId: "G-KGGHFEJQGW",
+};
+
+export default firebaseConfig;
