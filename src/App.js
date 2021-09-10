b8af688bfd1a2e39c04d87d9cf9524d0d14b7d69
Raaya <rahulagre555@gmail.com>
2021-09-10 13:08:39 +0530

    created sidebar input field and styled it

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import Sidebar from "./Sidebar";
 function App() {
   return (
     <div className="app">
-      {/* Sidebar */}
-      <Sidebar />
-      {/* Chat Window */}
-      <Chat />
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
