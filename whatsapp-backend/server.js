eb0b03a391ce7555eb40eb5aa6974bbf09c72384
Raaya <rahulagre555@gmail.com>
2021-09-14 21:16:20 +0530

    built the API

--- whatsapp-backend/server.js
+++ whatsapp-backend/server.js
@@ -1,12 +1,14 @@
 // importing
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 //app config
 const app = express();
 const port = process.env.PORT || 9000;
 
 //middleware
+app.use(express.json);
 
 //DB config
 const connection_url =
@@ -26,23 +28,24 @@ mongoose.connect(
 );
 */
 
-mongoose.connect(
-  connection_url,
-  {
-    useNewUrlParser: true,
-
-    useUnifiedTopology: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("Connected to MongoDB!!!");
-  }
-);
+mongoose.connect(connection_url);
 
 //???
 
 //api routes
 app.get("/", (req, res) => res.status(200).send("hello world"));
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
 //listen
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
